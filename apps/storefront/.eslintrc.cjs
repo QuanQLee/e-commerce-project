98eda803ce29ae62552d696ef298357bcdefce28
Initial commit <lizhihong268810@gmail.com>
2025-09-05 16:29:01 +0800

    fix DI/CD6.0

--- apps/storefront/.eslintrc.cjs
+++ apps/storefront/.eslintrc.cjs
@@ -8,8 +8,15 @@ module.exports = {
     'eslint:recommended',
     'plugin:react/recommended',
     'plugin:@typescript-eslint/recommended',
+    // Use Next.js recommended rules including React 17+ JSX transform support
+    'next/core-web-vitals',
     'prettier',
   ],
   settings: { react: { version: 'detect' } },
+  rules: {
+    // React 17+ JSX transform doesn't require React in scope
+    'react/react-in-jsx-scope': 'off',
+    // Prefer warnings for gradual typing improvements
+    '@typescript-eslint/no-explicit-any': 'warn',
+  },
 }
-
--- apps/storefront/.next/standalone/src/pages/_app.tsx
+++ apps/storefront/.next/standalone/src/pages/_app.tsx
@@ -1,14 +1,16 @@
 import type { AppProps } from 'next/app'
 import { CssBaseline, ThemeProvider, createTheme } from '@mui/material'
+import ErrorBoundary from '../components/ErrorBoundary'
 
 const theme = createTheme({ palette: { mode: 'light' } })
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <ThemeProvider theme={theme}>
-      <CssBaseline />
-      <Component {...pageProps} />
-    </ThemeProvider>
+    <ErrorBoundary>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </ErrorBoundary>
   )
 }
-
