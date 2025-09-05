f72c449032099d0aa28dd6b5e96cc6a059997664
Initial commit <lizhihong268810@gmail.com>
2025-09-06 00:30:36 +0800

    6.2

--- apps/storefront/next.config.js
+++ apps/storefront/next.config.js
@@ -1,7 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  output: 'standalone'
+  output: 'standalone',
+  eslint: {
+    // Avoid CI failures from lint surprises during docker builds
+    ignoreDuringBuilds: true,
+  },
 }
 module.exports = nextConfig
-
