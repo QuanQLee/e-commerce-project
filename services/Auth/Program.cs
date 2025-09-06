780fd413f69c959f11108b025ecd689e7593e496
Initial commit <lizhihong268810@gmail.com>
2025-09-06 23:15:46 +0800

    fix 6.4

--- services/Auth/Program.cs
+++ services/Auth/Program.cs
@@ -5,6 +5,7 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Http;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Configuration;
